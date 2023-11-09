6a103c4e65ac0973b428d27c45d3c7c10fdfdb16
OMARWAGDY <OMARWAGDY122@gmail.com>
2023-11-10 00:16:31 +0300

    v2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let HeaderCenter =document.querySelector(".header-center")
+
+let burger = document.querySelector(".burger")
+
+
+burger.addEventListener("click",()=>{
+
+    HeaderCenter.classList.toggle("hide")
+
+
+})
